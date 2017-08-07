5f9d898d30c97152b9b1e53a655c4a9c209554b7
Gufran <abdurraqeeb53@gmail.com>
2017-08-07 17:42:38 +1000

    MULTIPAGE EMBED

--- ext/formatter.py
+++ ext/formatter.py
@@ -395,7 +395,7 @@ class EmbedHelp(HelpFormatter):
         embeds = []
 
 
-        categs_per_page = 3 # change this value if you want to edit how many categories in one page
+        categs_per_page = 5 # change this value if you want to edit how many categories in one page
 
         for i in range(len(categs)):
             if i % categs_per_page == 0:
