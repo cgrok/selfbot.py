c2b1b7b9181c106cf77bc74262be32ee3be8ae62
FloatCobra <FloatCobra@users.noreply.github.com>
2017-08-14 16:58:37 +0100

    Updated things
    Moved 'eval' to bot.py
    added coglist command

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import discord
+from ext.formatter import EmbedHelp
 from discord.ext import commands
+from contextlib import redirect_stdout
 import datetime
 import json
-from ext.formatter import EmbedHelp
 import inspect
 import os
+import glob
+import io
+import textwrap
+import traceback
 
 def run_wizard():
     print('------------------------------------------')
@@ -25,23 +30,20 @@ def run_wizard():
     print('------------------------------------------')
     print('Successfully saved your data!')
     print('------------------------------------------')
-    
 
+
+with open('data/config.json') as f:
+    if json.load(f)['FIRST']:
+        run_wizard()
 
 if 'TOKEN' in os.environ:
     heroku = True
     TOKEN = os.environ['TOKEN']
 else:
     with open('data/config.json') as f:
-        if json.load(f)['FIRST']:
-            run_wizard()
-    with open('data/config.json') as f:  
         TOKEN = json.load(f)["BOT"]['TOKEN']
 
 async def get_pre(bot, message):
-    if 'PREFIX' in os.environ:
-        return os.environ['PREFIX']
-        
     with open('data/config.json') as f:
         config = json.load(f)
     try:
@@ -72,8 +74,7 @@ async def on_ready():
           'User ID: {}\n'
           '------------------------------------------'
     	  .format(bot.user, bot.user.id))
-    if heroku:
-        print('Hosting on heroku.')
+
 
 
 
@@ -84,7 +85,7 @@ async def ping(ctx):
     await (await bot.ws.ping())
     now = datetime.datetime.now()
     ping = now - msgtime
-    pong = discord.Embed(title='Pong! Response Time:', 
+    pong = discord.Embed(title='Pong! Response Time:',
     					 description=str(ping.microseconds / 1000.0) + ' ms',
                          color=0x00ffff)
 
@@ -146,7 +147,112 @@ async def on_command_error(error, ctx):
            await bot.send_message(channel, msg)
            return
 
+@bot.command(pass_context=True)
+async def coglist(ctx):
+    def pagify(text, delims=["\n"], *, escape=True, shorten_by=8,
+               page_length=2000):
+        """DOES NOT RESPECT MARKDOWN BOXES OR INLINE CODE"""
+        in_text = text
+        if escape:
+            num_mentions = text.count("@here") + text.count("@everyone")
+            shorten_by += num_mentions
+        page_length -= shorten_by
+        while len(in_text) > page_length:
+            closest_delim = max([in_text.rfind(d, 0, page_length)
+                                 for d in delims])
+            closest_delim = closest_delim if closest_delim != -1 else page_length
+            if escape:
+                to_send = escape_mass_mentions(in_text[:closest_delim])
+            else:
+                to_send = in_text[:closest_delim]
+            yield to_send
+            in_text = in_text[closest_delim:]
 
+
+        yield in_text
+
+    def box(text, lang=""):
+        ret = "```{}\n{}\n```".format(lang, text)
+        return ret
+    loaded = [c.__module__.split(".")[1] for c in bot.cogs.values()]
+    # What's in the folder but not loaded is unloaded
+    def _list_cogs():
+          cogs = [os.path.basename(f) for f in glob.glob("cogs/*.py")]
+          return ["cogs." + os.path.splitext(f)[0] for f in cogs]
+    unloaded = [c.split(".")[1] for c in _list_cogs()
+                if c.split(".")[1] not in loaded]
+
+    if not unloaded:
+        unloaded = ["None"]
+
+    msg = ("+ Loaded\n"
+           "{}\n\n"
+           "- Unloaded\n"
+           "{}"
+           "".format(", ".join(sorted(loaded)),
+                     ", ".join(sorted(unloaded)))
+           )
+    for page in pagify(msg, [" "], shorten_by=16):
+        await bot.say(box(page.lstrip(" "), lang="diff"))
+
+def cleanup_code( content):
+    """Automatically removes code blocks from the code."""
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    # remove `foo`
+    return content.strip('` \n')
+
+def get_syntax_error(e):
+    if e.text is None:
+        return '```py\n{0.__class__.__name__}: {0}\n```'.format(e)
+    return '```py\n{0.text}{1:>{0.offset}}\n{2}: {0}```'.format(e, '^', type(e).__name__)
+
+@bot.command(pass_context=True, name='eval')
+async def _eval(ctx, *, body: str):
+    '''Run python scripts on discord!'''
+    env = {
+        'bot': bot,
+        'ctx': ctx,
+        'channel': ctx.message.channel,
+        'author': ctx.message.author,
+        'server': ctx.message.server,
+        'message': ctx.message,
+    }
+
+    env.update(globals())
+
+    body = cleanup_code(content=body)
+    stdout = io.StringIO()
+
+    to_compile = 'async def func():\n%s' % textwrap.indent(body, '  ')
+
+    try:
+        exec(to_compile, env)
+    except SyntaxError as e:
+        return await bot.say(get_syntax_error(e))
+
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        await bot.say('```py\n{}{}\n```'.format(value, traceback.format_exc()))
+    else:
+        value = stdout.getvalue()
+        try:
+            await bot.add_reaction(ctx.message, '\u2705')
+        except:
+            pass
+
+        if ret is None:
+            if value:
+                await bot.say('```py\n%s\n```' % value)
+        else:
+
+            await bot.say('```py\n%s%s\n```' % (value, ret))
 
 
 @bot.command(pass_context=True,name='reload')
@@ -185,25 +291,16 @@ async def unload(ctx, *, module):
     except:
         pass
 
+for extension in _extensions:
+    try:
+        bot.load_extension(extension)
+        print('Loaded: {}'.format(extension))
+    except Exception as e:
+        exc = '{}: {}'.format(type(e).__name__, e)
+        print('Error on load: {}\n{}'.format(extension, exc))
 
-if __name__ == "__main__":
-    for extension in _extensions:
-        try:
-            bot.load_extension(extension)
-            print('Loaded: {}'.format(extension))
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Error on load: {}\n{}'.format(extension, exc))
-
-try:   
-    bot.run(TOKEN.strip("\""), bot=False)
+try:
+    print("running!")
+    bot.run(TOKEN, bot=False)
 except Exception as e:
     print('\n[ERROR]: \n{}\n'.format(e))
-
-
-
-
-
-
-    
-    
---
+++ utils.py
@@ -0,0 +1,430 @@
+import discord
+from discord.ext import commands
+import asyncio
+import requests
+from bs4 import BeautifulSoup
+from urllib import parse
+from urllib.parse import parse_qs
+from urllib.request import Request, urlopen
+import inspect
+import aiohttp
+from lxml import etree
+
+class Utility:
+    def __init__(self, bot):
+        self.bot = bot
+        self.sessions = set()
+
+    @commands.command(aliases=['nick'], pass_context=True, no_pm=True)
+    async def nickname(self, ctx, *, nick):
+        """Change your nickname on a server."""
+        await self.bot.delete_message(ctx.message)
+        try:
+            await self.bot.change_nickname(ctx.message.author, nick)
+            await self.bot.say('Changed nickname to: `{}`'.format(nick), delete_after=5)
+        except:
+            await self.bot.say('Unable to change nickname.', delete_after=5)
+
+    @commands.command(pass_context=True)
+    async def raw(self, ctx, ID, chan : discord.channel=None):
+        """Get the raw content of someones message!"""
+        channel = chan or ctx.message.channel
+        await self.bot.delete_message(ctx.message)
+        msg = None
+        async for m in self.bot.logs_from(channel, limit=1000):
+            if m.id == ID:
+                msg = m
+                break
+        out = msg.content.replace('*','\\*').replace('`','\\`').replace('~~','\\~~').replace('_','\\_').replace('<','\\<').replace('>','\\>')
+        try:
+            await self.bot.say(out)
+        except:
+            await self.bot.say('Message too long.')
+
+
+    @commands.command(pass_context=True)
+    async def quote(self, ctx, id : str, chan : discord.Channel=None):
+        """Quote someone's message by ID"""
+        channel = chan or ctx.message.channel
+        await self.bot.delete_message(ctx.message)
+        msg = None
+        async for message in self.bot.logs_from(channel, limit=1000):
+            if message.id == id:
+                msg = message
+                break
+        if msg is None:
+            await self.bot.say('Could not find the message.')
+            return
+        auth = msg.author
+        channel = msg.channel
+        ts = msg.timestamp
+        em = discord.Embed(color=0x00FFFF,description=msg.clean_content,timestamp=ts)
+        em.set_author(name=str(auth),icon_url=auth.avatar_url or auth.default_avatar_url)
+        try:
+            em.set_footer(text='#'+channel.name)
+        except: pass
+        await self.bot.say(embed=em)
+
+    @commands.command(pass_context=True, aliases=['yt', 'vid', 'video'])
+    async def youtube(self, ctx, *, msg):
+        """Search for videos on YouTube."""
+        search = parse.quote(msg)
+        response = requests.get("https://www.youtube.com/results?search_query={}".format(search)).text
+        result = BeautifulSoup(response, "lxml")
+        url="**Result:**\nhttps://www.youtube.com{}".format(result.find_all(attrs={'class': 'yt-uix-tile-link'})[0].get('href'))
+
+        await self.bot.send_message(ctx.message.channel, url)
+
+    @commands.command(pass_context=True,description='Do .embed to see how to use it.')
+    async def embed(self, ctx, *, msg: str = None):
+        '''Embed complex rich embeds as the bot.'''
+        try:
+
+            if msg:
+                ptext = title = description = image = thumbnail = color = footer = author = None
+                timestamp = discord.Embed.Empty
+                def_color = False
+                embed_values = msg.split('|')
+                for i in embed_values:
+                    if i.strip().lower().startswith('ptext='):
+                        if i.strip()[6:].strip() == 'everyone':
+                            ptext = '@everyone'
+                        elif i.strip()[6:].strip() == 'here':
+                            ptext = '@here'
+                        else:
+                            ptext = i.strip()[6:].strip()
+                    elif i.strip().lower().startswith('title='):
+                        title = i.strip()[6:].strip()
+                    elif i.strip().lower().startswith('description='):
+                        description = i.strip()[12:].strip()
+                    elif i.strip().lower().startswith('desc='):
+                        description = i.strip()[5:].strip()
+                    elif i.strip().lower().startswith('image='):
+                        image = i.strip()[6:].strip()
+                    elif i.strip().lower().startswith('thumbnail='):
+                        thumbnail = i.strip()[10:].strip()
+                    elif i.strip().lower().startswith('colour='):
+                        color = i.strip()[7:].strip()
+                    elif i.strip().lower().startswith('color='):
+                        color = i.strip()[6:].strip()
+                    elif i.strip().lower().startswith('footer='):
+                        footer = i.strip()[7:].strip()
+                    elif i.strip().lower().startswith('author='):
+                        author = i.strip()[7:].strip()
+                    elif i.strip().lower().startswith('timestamp'):
+                        timestamp = ctx.message.timestamp
+
+                    if color:
+                        if color.startswith('#'):
+                            color = color[1:]
+                        if not color.startswith('0x'):
+                            color = '0x' + color
+
+                    if ptext is title is description is image is thumbnail is color is footer is author is None and 'field=' not in msg:
+                        await self.bot.delete_message(ctx.message)
+                        return await self.bot.send_message(ctx.message.channel, content=None,
+                                                           embed=discord.Embed(description=msg))
+
+                    if color:
+                        em = discord.Embed(timestamp=timestamp, title=title, description=description, color=int(color, 16))
+                    else:
+                        em = discord.Embed(timestamp=timestamp, title=title, description=description)
+                    for i in embed_values:
+                        if i.strip().lower().startswith('field='):
+                            field_inline = True
+                            field = i.strip().lstrip('field=')
+                            field_name, field_value = field.split('value=')
+                            if 'inline=' in field_value:
+                                field_value, field_inline = field_value.split('inline=')
+                                if 'false' in field_inline.lower() or 'no' in field_inline.lower():
+                                    field_inline = False
+                            field_name = field_name.strip().lstrip('name=')
+                            em.add_field(name=field_name, value=field_value.strip(), inline=field_inline)
+                    if author:
+                        if 'icon=' in author:
+                            text, icon = author.split('icon=')
+                            if 'url=' in icon:
+                                print("here")
+                                em.set_author(name=text.strip()[5:], icon_url=icon.split('url=')[0].strip(), url=icon.split('url=')[1].strip())
+                            else:
+                                em.set_author(name=text.strip()[5:], icon_url=icon)
+                        else:
+                            if 'url=' in author:
+                                print("here")
+                                em.set_author(name=author.split('url=')[0].strip()[5:], url=author.split('url=')[1].strip())
+                            else:
+                                em.set_author(name=author)
+
+                    if image:
+                        em.set_image(url=image)
+                    if thumbnail:
+                        em.set_thumbnail(url=thumbnail)
+                    if footer:
+                        if 'icon=' in footer:
+                            text, icon = footer.split('icon=')
+                            em.set_footer(text=text.strip()[5:], icon_url=icon)
+                        else:
+                            em.set_footer(text=footer)
+                await self.bot.send_message(ctx.message.channel, content=ptext, embed=em)
+            else:
+                msg = '*Params:*\n```bf\n[title][author][desc][field][footer][thumbnail][image][timestamp][ptext]```'
+                await self.bot.send_message(ctx.message.channel, msg)
+            try:
+                await self.bot.delete_message(ctx.message)
+            except:
+                pass
+        except:
+            await self.bot.send_message(ctx.message.channel, 'looks like something fucked up. or i dont have embed perms')
+
+
+    def parse_google_card(self, node):
+        if node is None:
+            return None
+
+        e = discord.Embed(colour=0x00FFFF)
+
+        # check if it's a calculator card:
+        calculator = node.find(".//table/tr/td/span[@class='nobr']/h2[@class='r']")
+        if calculator is not None:
+            e.title = 'Calculator'
+            e.description = ''.join(calculator.itertext())
+            return e
+
+        parent = node.getparent()
+
+        # check for unit conversion card
+        unit = parent.find(".//ol//div[@class='_Tsb']")
+        if unit is not None:
+            e.title = 'Unit Conversion'
+            e.description = ''.join(''.join(n.itertext()) for n in unit)
+            return e
+
+        # check for currency conversion card
+        currency = parent.find(".//ol/table[@class='std _tLi']/tr/td/h2")
+        if currency is not None:
+            e.title = 'Currency Conversion'
+            e.description = ''.join(currency.itertext())
+            return e
+
+        # check for release date card
+        release = parent.find(".//div[@id='_vBb']")
+        if release is not None:
+            try:
+                e.description = ''.join(release[0].itertext()).strip()
+                e.title = ''.join(release[1].itertext()).strip()
+                return e
+            except:
+                return None
+
+        # check for definition card
+        words = parent.find(".//ol/div[@class='g']/div/h3[@class='r']/div")
+        if words is not None:
+            try:
+                definition_info = words.getparent().getparent()[1] # yikes
+            except:
+                pass
+            else:
+                try:
+                    # inside is a <div> with two <span>
+                    # the first is the actual word, the second is the pronunciation
+                    e.title = words[0].text
+                    e.description = words[1].text
+                except:
+                    return None
+
+                # inside the table there's the actual definitions
+                # they're separated as noun/verb/adjective with a list
+                # of definitions
+                for row in definition_info:
+                    if len(row.attrib) != 0:
+                        # definitions are empty <tr>
+                        # if there is something in the <tr> then we're done
+                        # with the definitions
+                        break
+
+                    try:
+                        data = row[0]
+                        lexical_category = data[0].text
+                        body = []
+                        for index, definition in enumerate(data[1], 1):
+                            body.append('%s. %s' % (index, definition.text))
+
+                        e.add_field(name=lexical_category, value='\n'.join(body), inline=False)
+                    except:
+                        continue
+
+                return e
+
+        # check for "time in" card
+        time_in = parent.find(".//ol//div[@class='_Tsb _HOb _Qeb']")
+        if time_in is not None:
+            try:
+                time_place = ''.join(time_in.find("span[@class='_HOb _Qeb']").itertext()).strip()
+                the_time = ''.join(time_in.find("div[@class='_rkc _Peb']").itertext()).strip()
+                the_date = ''.join(time_in.find("div[@class='_HOb _Qeb']").itertext()).strip()
+            except:
+                return None
+            else:
+                e.title = time_place
+                e.description = '%s\n%s' % (the_time, the_date)
+                return e
+
+        # check for weather card
+        # this one is the most complicated of the group lol
+        # everything is under a <div class="e"> which has a
+        # <h3>{{ weather for place }}</h3>
+        # string, the rest is fucking table fuckery.
+        weather = parent.find(".//ol//div[@class='e']")
+        if weather is None:
+            return None
+
+        location = weather.find('h3')
+        if location is None:
+            return None
+
+        e.title = ''.join(location.itertext())
+
+        table = weather.find('table')
+        if table is None:
+            return None
+
+        # This is gonna be a bit fucky.
+        # So the part we care about is on the second data
+        # column of the first tr
+        try:
+            tr = table[0]
+            img = tr[0].find('img')
+            category = img.get('alt')
+            image = 'https:' + img.get('src')
+            temperature = tr[1].xpath("./span[@class='wob_t']//text()")[0]
+        except:
+            return None # RIP
+        else:
+            e.set_thumbnail(url=image)
+            e.description = '*%s*' % category
+            e.add_field(name='Temperature', value=temperature)
+
+        # On the 4th column it tells us our wind speeds
+        try:
+            wind = ''.join(table[3].itertext()).replace('Wind: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Wind', value=wind)
+
+        # On the 5th column it tells us our humidity
+        try:
+            humidity = ''.join(table[4][0].itertext()).replace('Humidity: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Humidity', value=humidity)
+
+        return e
+
+    async def get_google_entries(self, query):
+        params = {
+            'q': query,
+            'safe': 'on',
+            'lr': 'lang_en',
+            'hl': 'en'
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)'
+        }
+
+        # list of URLs
+        entries = []
+
+        # the result of a google card, an embed
+        card = None
+
+        async with aiohttp.get('https://www.google.com.au/search', params=params, headers=headers) as resp:
+            if resp.status != 200:
+                raise RuntimeError('Google somehow failed to respond.')
+
+            root = etree.fromstring(await resp.text(), etree.HTMLParser())
+
+            # with open('google.html', 'w', encoding='utf-8') as f:
+            #     f.write(etree.tostring(root, pretty_print=True).decode('utf-8'))
+
+            """
+            Tree looks like this.. sort of..
+            <div class="g">
+                ...
+                <h3>
+                    <a href="/url?q=<url>" ...>title</a>
+                </h3>
+                ...
+                <span class="st">
+                    <span class="f">date here</span>
+                    summary here, can contain <em>tag</em>
+                </span>
+            </div>
+            """
+
+            card_node = root.find(".//div[@id='topstuff']")
+            card = self.parse_google_card(card_node)
+
+            search_nodes = root.findall(".//div[@class='g']")
+            for node in search_nodes:
+                url_node = node.find('.//h3/a')
+                if url_node is None:
+                    continue
+
+                url = url_node.attrib['href']
+                if not url.startswith('/url?'):
+                    continue
+
+                url = parse_qs(url[5:])['q'][0] # get the URL from ?q query string
+
+                # if I ever cared about the description, this is how
+                entries.append(url)
+
+                # short = node.find(".//span[@class='st']")
+                # if short is None:
+                #     entries.append((url, ''))
+                # else:
+                #     text = ''.join(short.itertext())
+                #     entries.append((url, text.replace('...', '')))
+
+        return card, entries
+
+    @commands.command(aliases=['g'])
+    async def google(self, *, query):
+        """Searches google and gives you top result."""
+        await self.bot.type()
+        try:
+            card, entries = await self.get_google_entries(query)
+        except RuntimeError as e:
+            await self.bot.say(str(e))
+        else:
+            if card:
+                value = '\n'.join(entries[:3])
+                if value:
+                    card.add_field(name='Search Results', value=value, inline=False)
+                return await self.bot.say(embed=card)
+
+            if len(entries) == 0:
+                return await self.bot.say('No results found... sorry.')
+
+            next_two = entries[1:3]
+            first_entry = entries[0]
+            if first_entry[-1] == ')':
+                first_entry = first_entry[:-1] + '%29'
+
+            if next_two:
+                formatted = '\n'.join(map(lambda x: '<%s>' % x, next_two))
+                msg = '{}\n\n**See also:**\n{}'.format(first_entry, formatted)
+            else:
+                msg = first_entry
+
+            await self.bot.say(msg)
+
+    @commands.command(pass_context=True)
+    async def source(self, ctx, *, command):
+        '''See the source code for any command.'''
+        await self.bot.say('```py\n'+str(inspect.getsource(self.bot.get_command(command).callback)+'```'))
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
