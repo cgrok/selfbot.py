575ad35a50493f66dc73e6671f4d67be111f9312
FloatCobra <FloatCobra@users.noreply.github.com>
2017-08-30 11:47:34 +0100

    added eval cmd

--- bot.py
+++ bot.py
@@ -97,5 +97,92 @@ class SelfBot(commands.Bot):
 
         await ctx.send(embed=pong)
 
+    def cleanup_code( content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    def get_syntax_error(e):
+        if e.text is None:
+            return '```py\n{0.__class__.__name__}: {0}\n```'.format(e)
+        return '```py\n{0.text}{1:>{0.offset}}\n{2}: {0}```'.format(e, '^', type(e).__name__)
+
+    async def to_code_block(ctx, body):
+        if body.startswith('```') and body.endswith('```'):
+            content = '\n'.join(body.split('\n')[1:-1])
+        else:
+            content = body.strip('`')
+        await ctx.message.edit(content='```py\n'+content+'```')
+
+    @commands.command(name='eval')
+    async def _eval(ctx, *, body: str):
+        '''Run python scripts on discord!'''
+        await to_code_block(ctx, body)
+        env = {
+            'bot': bot,
+            'ctx': ctx,
+            'channel': ctx.message.channel,
+            'author': ctx.message.author,
+            'server': ctx.message.server,
+            'message': ctx.message,
+        }
+
+        env.update(globals())
+
+        body = cleanup_code(content=body)
+        stdout = io.StringIO()
+
+        to_compile = 'async def func():\n%s' % textwrap.indent(body, '  ')
+
+        try:
+            exec(to_compile, env)
+        except SyntaxError as e:
+            return await ctx.send(get_syntax_error(e))
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            x = await ctx.send('```py\n{}{}\n```'.format(value, traceback.format_exc()))
+            try:
+                await x.add_reaction('\U0001f534')
+            except:
+                pass
+        else:
+            value = stdout.getvalue()
+
+            if TOKEN in value:
+                value = value.replace(TOKEN,"[EXPUNGED]")
+
+            if ret is None:
+                if value:
+                    try:
+                        x = await ctx.send('```py\n%s\n```' % value)
+                    except:
+                        x = await ctx.send('```py\n\'Result was too long.\'```')
+                    try:
+                        await x.add_reaction('\U0001f535')
+                    except:
+                        pass
+                else:
+                    try:
+                        await ctx.message.add_reaction('\U0001f535')
+                    except:
+                        pass
+            else:
+                try:
+                    x = await ctx.send('```py\n%s%s\n```' % (value, ret))
+                except:
+                    x = await ctx.send('```py\n\'Result was too long.\'```')
+                try:
+                    await x.add_reaction('\U0001f535')
+                except:
+                    pass
 if __name__ == '__main__':
     SelfBot().run()
