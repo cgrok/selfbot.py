723178d4bd9ab6d50d8e000c5951739f96e18e88
verixx <abdurraqeeb53@gmail.com>
2017-09-01 15:29:57 +1000

    Added google command / cog

---
+++ cogs/google.py
@@ -0,0 +1,217 @@
+import discord
+from discord.ext import commands
+
+from urllib.parse import parse_qs
+from lxml import etree
+
+
+class Google:
+    '''Copied from R.Danny'''
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['g'])
+    async def google(self ,ctx, *, query):
+        """Searches google and gives you top result."""
+        await ctx.trigger_typing()
+        try:
+            card, entries = await self.get_google_entries(ctx, query)
+        except RuntimeError as e:
+            await ctx.send(str(e))
+        else:
+            if card:
+                value = '\n'.join(entries[:3])
+                if value:
+                    card.add_field(name='Search Results', value=value, inline=False)
+                return await ctx.send(embed=card)
+
+            if len(entries) == 0:
+                return await ctx.send('No results found... sorry.')
+
+            next_two = entries[1:3]
+            first_entry = entries[0]
+            if first_entry[-1] == ')':
+                first_entry = first_entry[:-1] + '%29'
+
+            if next_two:
+                formatted = '\n'.join(map(lambda x: '<%s>' % x, next_two))
+                msg = '{}\n\n**See also:**\n{}'.format(first_entry, formatted)
+            else:
+                msg = first_entry
+
+            await ctx.send(msg)
+
+   def parse_google_card(self, node):
+        if node is None:
+            return None
+
+        e = discord.Embed(colour=0x00FFFF)
+
+        # check if it's a calculator card:
+        calculator = node.find(".//table/tr/td/span[@class='nobr']/h2[@class='r']")
+        if calculator is not None:
+            e.title = 'Calculator'
+            e.description = ''.join(calculator.itertext())
+            return e
+
+        parent = node.getparent()
+
+        # check for unit conversion card
+        unit = parent.find(".//ol//div[@class='_Tsb']")
+        if unit is not None:
+            e.title = 'Unit Conversion'
+            e.description = ''.join(''.join(n.itertext()) for n in unit)
+            return e
+
+        # check for currency conversion card
+        currency = parent.find(".//ol/table[@class='std _tLi']/tr/td/h2")
+        if currency is not None:
+            e.title = 'Currency Conversion'
+            e.description = ''.join(currency.itertext())
+            return e
+
+        # check for release date card
+        release = parent.find(".//div[@id='_vBb']")
+        if release is not None:
+            try:
+                e.description = ''.join(release[0].itertext()).strip()
+                e.title = ''.join(release[1].itertext()).strip()
+                return e
+            except:
+                return None
+
+        # check for definition card
+        words = parent.find(".//ol/div[@class='g']/div/h3[@class='r']/div")
+        if words is not None:
+            try:
+                definition_info = words.getparent().getparent()[1] # yikes
+            except:
+                pass
+            else:
+                try:
+                    e.title = words[0].text
+                    e.description = words[1].text
+                except:
+                    return None
+
+                for row in definition_info:
+                    if len(row.attrib) != 0:
+                        break
+                    try:
+                        data = row[0]
+                        lexical_category = data[0].text
+                        body = []
+                        for index, definition in enumerate(data[1], 1):
+                            body.append('%s. %s' % (index, definition.text))
+
+                        e.add_field(name=lexical_category, value='\n'.join(body), inline=False)
+                    except:
+                        continue
+
+                return e
+
+        # check for "time in" card
+        time_in = parent.find(".//ol//div[@class='_Tsb _HOb _Qeb']")
+        if time_in is not None:
+            try:
+                time_place = ''.join(time_in.find("span[@class='_HOb _Qeb']").itertext()).strip()
+                the_time = ''.join(time_in.find("div[@class='_rkc _Peb']").itertext()).strip()
+                the_date = ''.join(time_in.find("div[@class='_HOb _Qeb']").itertext()).strip()
+            except:
+                return None
+            else:
+                e.title = time_place
+                e.description = '%s\n%s' % (the_time, the_date)
+                return e
+
+        weather = parent.find(".//ol//div[@class='e']")
+        if weather is None:
+            return None
+
+        location = weather.find('h3')
+        if location is None:
+            return None
+
+        e.title = ''.join(location.itertext())
+
+        table = weather.find('table')
+        if table is None:
+            return None
+
+        try:
+            tr = table[0]
+            img = tr[0].find('img')
+            category = img.get('alt')
+            image = 'https:' + img.get('src')
+            temperature = tr[1].xpath("./span[@class='wob_t']//text()")[0]
+        except:
+            return None # RIP
+        else:
+            e.set_thumbnail(url=image)
+            e.description = '*%s*' % category
+            e.add_field(name='Temperature', value=temperature)
+
+        # On the 4th column it tells us our wind speeds
+        try:
+            wind = ''.join(table[3].itertext()).replace('Wind: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Wind', value=wind)
+
+        # On the 5th column it tells us our humidity
+        try:
+            humidity = ''.join(table[4][0].itertext()).replace('Humidity: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Humidity', value=humidity)
+
+        return e
+
+    async def get_google_entries(self, ctx, query):
+        params = {
+            'q': query,
+            'safe': 'on',
+            'lr': 'lang_en',
+            'hl': 'en'
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)'
+        }
+
+        # list of URLs
+        entries = []
+
+        # the result of a google card, an embed
+        card = None
+
+        async with ctx.session.get('https://www.google.com/search', params=params, headers=headers) as resp:
+            if resp.status != 200:
+                raise RuntimeError('Google somehow failed to respond.')
+
+            root = etree.fromstring(await resp.text(), etree.HTMLParser())
+
+            card_node = root.find(".//div[@id='topstuff']")
+            card = self.parse_google_card(card_node)
+
+            search_nodes = root.findall(".//div[@class='g']")
+            for node in search_nodes:
+                url_node = node.find('.//h3/a')
+                if url_node is None:
+                    continue
+
+                url = url_node.attrib['href']
+                if not url.startswith('/url?'):
+                    continue
+
+                url = parse_qs(url[5:])['q'][0] 
+
+                entries.append(url)
+
+        return card, entries
+
+
+def setup(bot):
+	bot.add_cog(Google(bot))
+
