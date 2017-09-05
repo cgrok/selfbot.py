70d819471ac25c72524c0b4577dfcccc7aa1dd05
verixx <abdurraqeeb53@gmail.com>
2017-09-06 08:25:39 +1000

    Added get_message to the CustomContext
    useful for selfbots

--- ext/context.py
+++ ext/context.py
@@ -3,10 +3,25 @@ from discord.ext import commands
 import asyncio
 
 class CustomContext(commands.Context):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-	@property
-	def session(self):
-		return self.bot.session
+    @property
+    def session(self):
+        return self.bot.session
+
+    async def _get_message(self, channel, id):
+        async for message in channel.history(limit=10000):
+            if message.id == id:
+                return message
+
+    async def get_message(self, channel_or_id, id=None):
+        '''Helper tool to get a message for selfbots'''
+        if isinstance(channel_or_id, int):
+            msg = self._get_message(channel=self.channel, id=channel_or_id)
+        else:
+            msg = self._get_message(channel=channel_or_id, id=id)
+        return msg
+
+
 		
