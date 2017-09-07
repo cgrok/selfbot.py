a3ce812b79e390877944d61d7255b008b4f532fb
verixx <abdurraqeeb53@gmail.com>
2017-09-07 18:32:18 +1000

    Added save_json helper

--- ext/utility.py
+++ ext/utility.py
@@ -3,3 +3,8 @@ import json
 def load_json(path):
     with open(path) as f:
         return json.load(f)
+
+def save_json(data, path, **kwargs):
+    with open(path, 'w') as f:
+        json_data = json.dumps(data, indent=4, **kwargs)
+        return f.write(json_data)
