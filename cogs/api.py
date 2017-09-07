3a4587420ca1e51e6a94fdf9e1f6a894e83bad13
verixx <abdurraqeeb53@gmail.com>
2017-09-07 17:02:55 +1000

    Added license

--- cogs/api.py
+++ cogs/api.py
@@ -1,3 +1,27 @@
+'''
+MIT License
+
+Copyright (c) 2017 verixx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 import discord
 from discord.ext import commands
 from ext import fuzzy
@@ -5,14 +29,11 @@ from urllib.parse import parse_qs
 from lxml import etree
 import re
 
-'''This command is made by danny!'''
-
-
 class ApiCog:
+    '''Code is written by Rapptz from R.Danny'''
 
     def __init__(self, bot):
         self.bot = bot
-
 
     async def build_rtfm_lookup_table(self):
         cache = {}
--- cogs/utils.py
+++ cogs/utils.py
@@ -1,3 +1,27 @@
+'''
+MIT License
+
+Copyright (c) 2017 verixx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import TextChannelConverter
@@ -27,10 +51,12 @@ class Utility:
             return await ctx.send('Could not find that message!', delete_after=3.0)
 
         em = discord.Embed(color=0x00FFFF, description=msg.clean_content, timestamp=msg.created_at)
-        em.set_author(name=str(msg.author), icon_url=msg.author.avatar_url or msg.author.default_avatar_url)
+        em.set_author(name=str(msg.author), icon_url=msg.author.avatar_url)
 
         if isinstance(msg.channel, discord.TextChannel):
             em.set_footer(text='#'+str(msg.channel))
+        else:
+            em.set_footer(text=str(msg.channel))
 
         await ctx.send(embed=em)
 
