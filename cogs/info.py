395554dd31fa5540c436b3a4c98a8b772638c000
verixx <abdurraqeeb53@gmail.com>
2017-09-10 16:50:38 +1000

    Started on info commands

---
+++ cogs/info.py
@@ -0,0 +1,94 @@
+'''
+MIT License
+
+Copyright (c) 2017 verixx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import discord
+from discord.ext import commands
+
+
+class Information:
+	def __init__(self, bot):
+		self.bot = bot
+
+    @commands.command(aliases=['server','si'])
+    @commands.guild_only()
+    async def serverinfo(self, ctx):
+        '''See information about the server.'''
+        server = ctx.server
+        total_users = len(server.members)
+        online = len([m for m in server.members if m.status != discord.Status.offline])
+        text_channels = len([x for x in server.channels if x.type == discord.ChannelType.text])
+        voice_channels = len(server.channels) - text_channels
+        passed = (ctx.message.created_at - server.created_at).days
+        created_at = "Since {}. That's over {} days ago!"
+                     .format(server.created_at.strftime("%d %b %Y %H:%M"),
+                                passed)
+
+        colour = random.randint(0, 0xFFFFFF)
+
+        data = discord.Embed(description=created_at,colour=colour)
+        data.add_field(name="Region", value=str(server.region))
+        data.add_field(name="Users", value="{}/{}".format(online, total_users))
+        data.add_field(name="Text Channels", value=text_channels)
+        data.add_field(name="Voice Channels", value=voice_channels)
+        data.add_field(name="Roles", value=len(server.roles))
+        data.add_field(name="Owner", value=str(server.owner))
+        data.set_footer(text="Server ID: " + server.id)
+        data.set_author(name=server.name, icon_url=None or server.icon_url)
+        data.set_thumbnail(url=None or server.icon_url)
+
+        await ctx.send(embed=data)
+
+    @commands.command(aliases=['ui'])
+    @commands.guild_only()
+    async def userinfo(self, ctx, *, member : commands.MemberConverter=None):
+    	'''Get information about a member of a server'''
+        server = ctx.server
+        user = user or ctx.message.author
+        avi = user.avatar_url
+        roles = sorted(user.roles, key=lambda c: c.position)
+
+        for role in roles:
+            if str(role.color) != "#000000":
+                color = int(str(role.color)[1:], 16)
+
+        rolenames = ', '.join([r.name for r in roles]) or 'None'
+        time = ctx.message.created_at
+        desc = '{0} is chilling in {1} mode.'.format(user.name, user.status)
+        member_number = sorted(server.members, key=lambda m: m.joined_at).index(user) + 1
+
+        em = discord.Embed(colour=color, description=desc, timestamp=time)
+        em.add_field(name='Nick', value=user.nick, inline=True)
+        em.add_field(name='Member No.',value=str(member_number),inline = True)
+        em.add_field(name='Account Created', value=user.created_at.__format__('%A, %d. %B %Y'))
+        em.add_field(name='Join Date', value=user.joined_at.__format__('%A, %d. %B %Y'))
+        em.add_field(name='Roles', value=rolenames, inline=True)
+        em.set_footer(text='User ID: '+str(user.id))
+        em.set_thumbnail(url=avi)
+        em.set_author(name=user, icon_url=ctx.server.icon_url)
+
+        await ctx.send(embed=em)
+
+
+def setup(bot):
+	bot.add_cog(Information(bot))
