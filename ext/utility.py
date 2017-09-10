8e565fbcd2273f08f3337de63e440e10a741d414
verixx <abdurraqeeb53@gmail.com>
2017-09-10 16:51:24 +1000

    Made load_json safer

--- ext/utility.py
+++ ext/utility.py
@@ -1,8 +1,11 @@
 import json
 
 def load_json(path):
-    with open(path) as f:
-        return json.load(f)
+	try:
+	    with open(path) as f:
+	        return json.load(f)
+	except:
+		return {}
 
 def save_json(data, path, **kwargs):
     with open(path, 'w') as f:
