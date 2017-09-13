6f9e06d1cc50ee33119c7672de7f4f9a81dbe374
verixx <abdurraqeeb53@gmail.com>
2017-09-13 19:53:08 +1000

    Added an eval command (R.Danny)

--- cogs/utils.py
+++ cogs/utils.py
@@ -33,8 +33,8 @@ from mtranslate import translate
 from urllib.parse import parse_qs
 from lxml import etree
 import inspect
-
 import re
+
 
 class Utility:
     '''Useful commands to make your life easier'''
@@ -507,6 +507,73 @@ class Utility:
                 entries.append(url)
 
         return card, entries
+
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        await self.edit_to_codeblock(ctx, body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
+
+    async def edit_to_codeblock(self, ctx, body):
+        msg = f'```py\n{body}\n```'
+        await ctx.message.edit(content=msg)
+
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    def get_syntax_error(self, e):
+        if e.text is None:
+            return f'```py\n{e.__class__.__name__}: {e}\n```'
+        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
 def setup(bot):
     bot.add_cog(Utility(bot))
