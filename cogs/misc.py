9a47b2f9be82047881ed9f372b4308b60085511e
kwugfighter <isaacli430@gmail.com>
2017-09-15 09:44:25 +0800

    urban

--- cogs/misc.py
+++ cogs/misc.py
@@ -167,8 +167,9 @@ class Misc:
             pos = 0
         search_terms = "+".join(search_terms)
         url = "http://api.urbandictionary.com/v0/define?term=" + search_terms
-        async with aiohttp.get(url) as r:
-            result = await r.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                result = await r.json()
         emb = discord.Embed()
         emb.color = await ctx.get_dominant_color(url=ctx.message.author.avatar_url)
         if result["list"]:
