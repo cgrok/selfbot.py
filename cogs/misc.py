e029f2cf9df95cf8e6d14f18328aeff051dfad49
kwugfighter <isaacli430@gmail.com>
2017-09-16 19:26:31 +0800

    fixed another bug

--- cogs/misc.py
+++ cogs/misc.py
@@ -123,11 +123,9 @@ class Misc:
             else:
                 c_pos += step
         return outputty
-        outputty = None
-        output = None
 
     @commands.command(pass_context=True, aliases=['bf'])
-    async def bf(self, ctx, slurp:str):
+    async def eval(self, ctx, slurp:str):
        thruput = ctx.message.content
        preinput = thruput[5:]
        preinput2 = "\"\"\"\n" + preinput
