3473b05fd2cb61e0daebc93096092370c6d80892
kwugfighter <isaacli430@gmail.com>
2017-10-02 20:24:05 +0800

    Reverted pagination because that broke eval

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,18 +1,14 @@
 '''
 MIT License
-
 Copyright (c) 2017 verixx
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -719,17 +715,13 @@ class Utility:
                     try:
                         out = await ctx.send(f'```py\n{value}\n```')
                     except:
-                        paginated_text = paginate(f"{value}")
-                        for page in paginated_text:
-                            out = await ctx.send(f'```py{page}```')
+                        out = await ctx.send('Result was too long to send.')
             else:
                 self._last_result = ret
                 try:
                     out = await ctx.send(f'```py\n{value}{ret}\n```')
                 except:
-                    paginated_text = paginate(f"{value}{ret}")
-                    for page in paginated_text:
-                        out = await ctx.send(f'```py\n{page}\n```')
+                    out = await ctx.send('Result was too long to send.')
 
         if out:
             await out.add_reaction('\u2705')
@@ -740,16 +732,6 @@ class Utility:
     async def edit_to_codeblock(self, ctx, body):
         msg = f'```py\n{body}\n```'
         await ctx.message.edit(content=msg)
-
-    def paginate(text: str):
-    '''Simple generator that paginates text.'''
-    last = 0
-    for curr in range(0, len(text)+1980, 1980):
-        if last == curr:
-            continue
-        else:
-            yield text[last:curr]
-            last = curr
 
     def cleanup_code(self, content):
         """Automatically removes code blocks from the code."""
