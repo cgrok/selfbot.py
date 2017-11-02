d61bb06d94e1da1879dccbedd8ac801e9d11309f
「XΛOS」 <30846531+XAOS1502@users.noreply.github.com>
2017-11-02 23:11:28 +0530

    calculator command!
    okay, I think it works, let me know if there's an issue

--- cogs/misc.py
+++ cogs/misc.py
@@ -22,14 +22,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+from __future__ import division
 import discord
-from discord.ext import commands
-from ext.utility import parse_equation
-from ext.colours import ColorNames
-from urllib.request import urlopen
-from sympy import solve
-from PIL import Image
-import asyncio
+import math
+import operator
+import colorthief
 import random
 import emoji
 import copy
@@ -39,12 +36,113 @@ import json
 import os
 import urllib.parse
 import urbanasync
+from datetime import datetime
+from discord.ext import commands
+from pyparsing import (Literal,CaselessLiteral,Word,Combine,Group,Optional,
+                    ZeroOrMore,Forward,nums,alphas,oneOf)
+from discord.ext import commands
+from ext.utility import parse_equation
+from ext.colours import ColorNames
+from urllib.request import urlopen
+from sympy import solve
+from PIL import Image
 
+
+class NumericStringParserForPython3(object):
+    '''
+    Most of this code comes from the fourFn.py pyparsing example
+
+    '''
+    def pushFirst(self, strg, loc, toks ):
+        self.exprStack.append( toks[0] )
+    def pushUMinus(self, strg, loc, toks ):
+        if toks and toks[0]=='-':
+            self.exprStack.append( 'unary -' )
+    def __init__(self):
+        """
+        Please use any of the following symbols:
+        expop   :: '^'
+        multop  :: '*' | '/'
+        addop   :: '+' | '-'
+        integer :: ['+' | '-'] '0'..'9'+
+        """
+        point = Literal( "." )
+        e     = CaselessLiteral( "E" )
+        fnumber = Combine( Word( "+-"+nums, nums ) +
+                        Optional( point + Optional( Word( nums ) ) ) +
+                        Optional( e + Word( "+-"+nums, nums ) ) )
+        ident = Word(alphas, alphas+nums+"_$")
+        plus  = Literal( "+" )
+        minus = Literal( "-" )
+        mult  = Literal( "*" )
+        div   = Literal( "/" )
+        lpar  = Literal( "(" ).suppress()
+        rpar  = Literal( ")" ).suppress()
+        addop  = plus | minus
+        multop = mult | div
+        expop = Literal( "^" )
+        pi    = CaselessLiteral( "PI" )
+        expr = Forward()
+        atom = ((Optional(oneOf("- +")) +
+                (pi|e|fnumber|ident+lpar+expr+rpar).setParseAction(self.pushFirst))
+                | Optional(oneOf("- +")) + Group(lpar+expr+rpar)
+                ).setParseAction(self.pushUMinus)
+        # by defining exponentiation as "atom [ ^ factor ]..." instead of
+        # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
+        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
+        factor = Forward()
+        factor << atom + ZeroOrMore( ( expop + factor ).setParseAction( self.pushFirst ) )
+        term = factor + ZeroOrMore( ( multop + factor ).setParseAction( self.pushFirst ) )
+        expr << term + ZeroOrMore( ( addop + term ).setParseAction( self.pushFirst ) )
+        # addop_term = ( addop + term ).setParseAction( self.pushFirst )
+        # general_term = term + ZeroOrMore( addop_term ) | OneOrMore( addop_term)
+        # expr <<  general_term
+        self.bnf = expr
+        # this will map operator symbols to their corresponding arithmetic operations
+        epsilon = 1e-12
+        self.opn = {
+                "+" : operator.add,
+                "-" : operator.sub,
+                "*" : operator.mul,
+                "/" : operator.truediv,
+                "^" : operator.pow }
+        self.fn  = {
+                "sin" : math.sin,
+                "cos" : math.cos,
+                "tan" : math.tan,
+                "abs" : abs,
+                "trunc" : lambda a: int(a),
+                "round" : round,
+                "sgn" : lambda a: abs(a)>epsilon and cmp(a,0) or 0}
+    def evaluateStack(self, s ):
+        op = s.pop()
+        if op == 'unary -':
+            return -self.evaluateStack( s )
+        if op in "+-*/^":
+            op2 = self.evaluateStack( s )
+            op1 = self.evaluateStack( s )
+            return self.opn[op]( op1, op2 )
+        elif op == "PI":
+            return math.pi # 3.1415926535
+        elif op == "E":
+            return math.e  # 2.718281828
+        elif op in self.fn:
+            return self.fn[op]( self.evaluateStack( s ) )
+        elif op[0].isalpha():
+            return 0
+        else:
+            return float( op )
+    def eval(self,num_string,parseAll=True):
+        self.exprStack=[]
+        results=self.bnf.parseString(num_string,parseAll)
+        val=self.evaluateStack( self.exprStack[:] )
+        return val
 
 class Misc:
     def __init__(self, bot):
         self.bot = bot
         self.emoji_converter = commands.EmojiConverter()
+        self.nsp=NumericStringParserForPython3()
 
     @commands.command()
     async def embedsay(self, ctx, *, message):
@@ -281,58 +379,51 @@ class Misc:
         emb.add_field(name="Shortened Link", value=new, inline=False)
         await ctx.send(embed=emb)
 
+    @commands.command(aliases=['calc', 'maths'])
+    async def calculate(self, ctx, *, formula=None):
+        """
+        Do some real math
+        finally a working command for mathematics
+        thanks to Paul McGuire's fourFn.py module
+        """
+        person = ctx.message.author
+        user = ctx.author
 
-    @commands.group(invoke_without_command=True, aliases=['calculate', 'calculator'])
-    async def calc(self, ctx):
-        """Basic Calculator [+ , - , / , x]"""
-        e = discord.Embed()
-        e.color = await ctx.get_dominant_color(url=ctx.message.author.avatar_url)
-        e.title = 'Usage:'
-        e.add_field(name='\N{DOWN-POINTING RED TRIANGLE} Add', value=f'```{ctx.prefix}calc + 2 5```', inline=True)
-        e.add_field(name='\N{DOWN-POINTING RED TRIANGLE} Rest', value=f'```{ctx.prefix}calc - 2 5```', inline=True)
-        e.add_field(name='\N{DOWN-POINTING RED TRIANGLE} Divide', value=f'```{ctx.prefix}calc / 2 5```', inline=True)
-        e.add_field(name='\N{DOWN-POINTING RED TRIANGLE} Multiply', value=f'```{ctx.prefix}calc x 2 5```', inline=True)
-        await ctx.channel.send(embed=e, delete_after=25)
+        if formula == None:
+            # How can it calculate an empty message? Reee!
+            msg = f'\u200BUsage: `{ctx.prefix}{ctx.invoked_with} [any maths formula]`'
+            e = discord.Embed()
+            e.color = await ctx.get_dominant_color(user.avatar_url)
+            e.description = f'{msg}'
+            await ctx.send(embed=e)
+            return
 
-    @calc.command(name='+')
-    async def _plus(self, ctx, *numbers: float):
-        """Adds two consecutive numbers separated by space"""
-        result = sum(numbers)
-        e = discord.Embed()
-        e.color = await ctx.get_dominant_color(url=ctx.message.author.avatar_url)
-        e.title = f'{ctx.message.author.display_name}'
-        e.description = f'Your answer is: `{result}`'
-        await ctx.channel.send(embed=e)
+        try:
+            answer=self.nsp.eval(formula)
+        except:
+            # If there's a problem in the input, show examples
+            msg = f'\N{THINKING FACE} wrong {formula} input.\nTry any of these:'
+            e = discord.Embed()
+            e.color = await ctx.get_dominant_color(user.avatar_url)
+            e.description = f'\u200B{msg}'
+            e.add_field(name='multiplication', value="`num` * `num`", inline=True)
+            e.add_field(name='division', value="`num` / `num`", inline=True)
+            e.add_field(name='addition', value="`num` + `num`", inline=True)
+            e.add_field(name='rest', value="`num` - `num`", inline=True)
+            e.add_field(name='exponential', value="`num` ^ `num`")
+            e.add_field(name='integer', 
+                        value="[`num` + `num` | `num` - `num`] `num` 0 `num`..`num` 9 `num` +")
+            await ctx.send(embed=e, delete_after=60)
+            return
 
-    @calc.command(name='-')
-    async def _minus(self, ctx, left: float, right: float):
-        """Substracts two consecutive numbers separated by space"""
-        result = left - right
+        # Correct input prints correct answer
+        msg = f'\N{INPUT SYMBOL FOR NUMBERS} {user.display_name}'
         e = discord.Embed()
-        e.color = await ctx.get_dominant_color(url=ctx.message.author.avatar_url)
-        e.title = f'{ctx.message.author.display_name}'
-        e.description = f'Your answer is: `{result}`'
-        await ctx.channel.send(embed=e)
-
-    @calc.command(name='x')
-    async def _multiply(self, ctx, left: float, right: float):
-        """Multiplies two consecutive numbers separated by space"""
-        result = left * right
-        e = discord.Embed()
-        e.color = await ctx.get_dominant_color(url=ctx.message.author.avatar_url)
-        e.title = f'{ctx.message.author.display_name}'
-        e.description = f'Your answer is: `{result}`'
-        await ctx.channel.send(embed=e)
-
-    @calc.command(name='/')
-    async def _divide(self, ctx, left: float, right: float):
-        """Divides two consecutive numbers separated by space"""
-        result = left / right
-        e = discord.Embed()
-        e.color = await ctx.get_dominant_color(url=ctx.message.author.avatar_url)
-        e.title = f'{ctx.message.author.display_name}'
-        e.description = f'Your answer is: `{result}`'
-        await ctx.channel.send(embed=e)
+        e.color = await ctx.get_dominant_color(user.avatar_url)
+        e.description = f'{msg}'
+        e.add_field(name='Input:', value="```{formula}```", inline=True)
+        e.add_field(name='Result:', value="```{round(answer, 2)}```", inline=True)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def algebra(self, ctx, *, equation):
