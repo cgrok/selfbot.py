beee1628f7e8eb953e965a045f33fbd7a94e4713
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-11-13 06:12:57 -0900

    Clash Royale Cog
    Added Clash Royale Cog Not Fully Ready But I'll Add More Later This Should Be Good For Now

--- cogs/community/clashroyale.py
+++ cogs/community/clashroyale.py
@@ -1,18 +1,14 @@
 '''
 MIT License
-
 Copyright (c) 2017 Grok
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,7 +38,7 @@ class ClashRoyale:
                 tag = config['CR_TAG']
         self.tag = os.environ.get('CR_TAG') or tag
         self.client = crasync.Client()
-
+    
 
     @commands.command()
     async def profile(self, ctx, tag=None):
@@ -54,22 +50,41 @@ class ClashRoyale:
             if tag == None:
                 em.description = "Please add `CR_TAG` to your config."
                 return await ctx.send(embed=em)
+        tag = tag.strip('#').replace('O', '0')
         try:
             profile = await self.client.get_profile(tag)
         except:
             em.description = "Either API is down or that's an invalid tag."
             return await ctx.send(embed=em)
 
+        trophies = str(profile.current_trophies)
+        pb = str(profile.highest_trophies)
+        xp = str(profile.level)
+        experience = str(profile.experience[0]) + '/' + str(profile.experience[1])
+        
         em.title = profile.name
+        em.set_thumbnail(url=profile.arena.image_url)
         em.description = f"#{tag}"
         em.url = f"http://cr-api.com/profile/{tag}"
+        em.add_field(name='Current Trophies', value=trophies)
+        em.add_field(name='Highest Trophies',value=pb)
+        em.add_field(name='Legend Trophies', value=f'{profile.legend_trophies}')
+        em.add_field(name='Level', value=xp)
+        em.add_field(name='Experience', value=experience)
+        em.add_field(name='Wins/Losses/Draws', value=f'{profile.wins}/{profile.losses}/{profile.draws}')
+        em.add_field(name='Global Rank', value=f'{profile.global_rank}')         
+        em.add_field(name='Clan Info', value=f'{profile.clan_name}' + '\n' + '#' + f'{profile.clan_tag}' + '\n' + f'{profile.clan_role}')
+        em.add_field(name='Win Streak', value=f'{profile.win_streak}')
+        em.set_footer(text="Powered By cr-api.com", icon_url="http://cr-api.com/static/img/branding/cr-api-logo.png")
+        
         try:
             em.set_author(name="Profile", icon_url=profile.clan_badge_url)
         except:
             em.set_author(name='Profile')
 
+      
         await ctx.send(embed=em) 
 
 
 def setup(bot):
-    bot.add_cog(ClashRoyale(bot))
+    bot.add_cog(ClashRoyale(bot))    
