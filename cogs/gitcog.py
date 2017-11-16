01802d6600e20feccd7c3147f4d5a2e2cd26df1f
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-11-16 22:53:06 +0800

    Have a localcheck to stop if git token is not provided, include a suggest com

--- cogs/gitcog.py
+++ cogs/gitcog.py
@@ -28,6 +28,8 @@ class Git:
         async with self.session.get('https://api.github.com/user', headers={"Authorization": f"Bearer {self.githubtoken}"}) as resp: #get username 
             if 300 > resp.status >= 200:
                 return (await resp.json())['login']
+            if resp.status == 401: #invalid token!
+                return None
 
     async def starred(self, repo):
         async with self.session.get('https://api.github.com/user/starred/' + repo, headers={"Authorization": f"Bearer {self.githubtoken}"}) as resp:
@@ -35,6 +37,12 @@ class Git:
                 return True
             if resp.status == 404:
                 return False
+        
+    async def __local_check(self, ctx):
+        if githubtoken is None:
+            await ctx.send('Github token not provided.', delete_after=10)
+            return False
+        return True
 
     @commands.command()
     async def issue(self, ctx, repo, issueid):
@@ -87,6 +95,20 @@ class Git:
             if resp.status != 200 and resp.status != 201:
                 return await ctx.send('ConnectionError: Github API Issue.')
         await ctx.send('Submitted comment to issue ' + str(issueid))
+
+    @commands.command()
+    async def suggest(self, ctx, summary, *, details):
+        '''Create an issue! `{p}makeissue <short summary> | <details>`'''
+        async with ctx.session.post('https://api.github.com/repos/verixx/selfbot.py/issues', json={"title": summary, "body": details}, headers={'Authorization': f'Bearer {self.githubtoken}'}) as resp:
+            if resp.status == 200 or resp.status == 201:
+                issueinfo = await resp.json()
+            else:
+                return await ctx.send('ConnectionError: Github API Issue.')
+
+        em = discord.Embed(title=issueinfo['title'] + ' (#' + str(issueinfo['number']) + ')', description=issueinfo['body'], url=issueinfo['html_url'], color=0xcb2431)
+        em.set_author(name=issueinfo['user']['login'] + ' (' + issueinfo['author_association'].capitalize() + ')', icon_url=issueinfo['user']['avatar_url'])
+        em.set_footer(text='Issue | ' + issueinfo['created_at'])
+        await ctx.send(embed=em)
 
 def setup(bot):
     bot.add_cog(Git(bot))
